440caf0a3bc9f04a0a314a74de93b3333490021c
petrasvensosn10 <petra.svensson@live.se>
2020-05-18 10:15:04 +0200

    checka in stack.js

--- src/stack.js
+++ src/stack.js
@@ -2,7 +2,7 @@ var _ = require('underscore');
 
 let stack = [];
 
-// Lägger ett element överst i stacken
+// Lägger ett element överst i stacken, 
 exports.push = function (x) {
     stack.push(x);
 };
